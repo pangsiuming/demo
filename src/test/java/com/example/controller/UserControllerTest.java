8d2b73894cfa708521d270d028cc15f924a5d211
Peng ZhaoMing <PengZhaoMing@aactechnologies.com>
2019-07-17 08:49:51 +0800

    add junit test

---
+++ src/test/java/com/example/controller/UserControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Test
+    public void createUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void removeUserById() {
+    }
+
+    @Test
+    public void findUserInfoByUsernameAndCname() {
+    }
+
+    @Test
+    public void findUserInfoById() {
+    }
+
+    @Test
+    public void findUserById() {
+    }
+
+    @Test
+    public void findAllUsers() {
+    }
+
+    @Test
+    public void findAllUserInfo() {
+    }
+}
